3002ad6707736187d68871691d46cf6b23ef3d65
Chang Huang <chhuang.cl@gmail.com>
2014-04-26 18:25:54 +1000

    Add _site

---
+++ _site/Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      dev: {
+        options: {
+          cleancss: true,
+          dumpLineNumbers: true
+        },
+        files: {
+          'assets/dist/css/core.css': 'assets/src/css/core.less'
+        }
+      }
+    },
+    
+    uglify: {
+      dev: {
+        files: {
+          'assets/dist/js/core.js': 'assets/src/js/core.js'
+        }
+      }
+    },
+
+    responsive_images: {
+      options: {
+        engine: 'im'
+      },
+      products: {
+        files: [
+          { expand:true, src: ['**/*.jpg'], cwd: 'assets/src/img/products/', dest: 'assets/dist/img/products/' }
+        ]
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: ['assets/**/*.less'],
+        tasks: ['less:dev']
+      },
+      js: {
+        files: ['assets/**/*.js'],
+        tasks: ['uglify:dev']
+      },
+      html: {
+        files: ['*.html']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['less:dev']);
+};
